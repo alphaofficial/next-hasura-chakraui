267ad1db5c154c1e1abe9d688fdc4836d20434b3
Albert Akrong <alphaxsalt@gmail.com>
2023-03-12 14:40:15 +0000

    add tests

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,7 +1,9 @@
-import { render } from "@/utils/test-utils";
+import { render, waitFor } from "@/utils/test-utils";
+import * as auth0 from "@auth0/nextjs-auth0/client";
 import Home from ".";
 
 const renderPage = () => render(<Home />);
+
 describe("Home page", () => {
   jest.mock("@tanstack/react-query", () => {
     const originalModule = jest.requireActual("@tanstack/react-query");
@@ -12,9 +14,48 @@ describe("Home page", () => {
     };
   });
 
-  it("Renders the heading", () => {
+  it("Renders the heading", async () => {
+    jest.spyOn(auth0, "useUser").mockImplementation(
+      () =>
+        ({
+          user: {
+            name: "John Doe",
+            email: "",
+          },
+          isLoading: false,
+        } as auth0.UserContext)
+    );
     const { getByLabelText } = renderPage();
-    const heading = getByLabelText("heading");
+    const heading = await waitFor(() => getByLabelText("heading"));
     expect(heading).toHaveTextContent("Hello graphql!");
+  });
+
+  it("should show the user's name if logged in", async () => {
+    jest.spyOn(auth0, "useUser").mockImplementation(
+      () =>
+        ({
+          user: {
+            name: "John Doe",
+            email: "",
+          },
+          isLoading: false,
+        } as auth0.UserContext)
+    );
+    const { getByLabelText } = renderPage();
+    const name = await waitFor(() => getByLabelText("authUser"));
+    expect(name).toHaveTextContent("John Doe");
+  });
+
+  it("should display login text if user is not logged in", async () => {
+    jest.spyOn(auth0, "useUser").mockImplementation(
+      () =>
+        ({
+          user: undefined,
+          isLoading: false,
+        } as auth0.UserContext)
+    );
+    const { getByLabelText } = renderPage();
+    const loginText = await waitFor(() => getByLabelText("loginText"));
+    expect(loginText).toHaveTextContent("Log in to see your followers");
   });
 });
